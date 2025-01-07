cc7557c54bc7537e26e11375cf23df9df62697f8
RaduTuodr <tudoreduard2004@gmail.com>
2025-01-07 21:35:12 +0200

    Updated for remote url

--- src/main/java/com/example/weathermonitor/config/CorsConfig.java
+++ src/main/java/com/example/weathermonitor/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://weather-monitor-jade.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
